1207a7e182bd615858bf54107fc943ae4f8397d6
Ema <ema_spirova@hotmail.com>
2021-10-10 19:33:40 +0200

    commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
+
   constructor() {
     setTimeout(()=>{
         this.allowNewServer = true;
@@ -19,4 +24,13 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus='Server was created! Name is ' + this.serverName ;
+  }
+
+  onUpdateServerName(event : Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
